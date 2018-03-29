2291141433e467624a4d24545102b786cf0266a4
n7n9 <37828438+n7n9@users.noreply.github.com>
2018-03-29 15:44:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,91 @@
+function getfile(file,callback){
+  var xhr=new XMLHttpRequest();
+  xhr.overrideMimeType("application/json");
+  xhr.open("GET",file,true);
+  xhr.onreadystatechange = function(){
+    if(xhr.readyState === 4 && xhr.status == "200")
+    {
+      callback(xhr.responseText);
+    }
+  };
+  xhr.send();
+}
+getfile("data.json",function(text){
+let data = JSON.parse(text);
+console.log(data);
+details(data.basics);
+career(data.career);
+education(data.education);
+});
+
+var parent = document.querySelector(".flex-parent");
+
+var child1 = document.createElement("div");
+child1.classList.add("profile");
+parent.appendChild(child1);
+
+var child2 = document.createElement("div");
+child2.classList.add("content");
+parent.appendChild(child2);
+
+function details(basic){
+  var img = document.createElement("img");
+  img.src = basic.photo;
+  child1.appendChild(img);
+
+  var name = document.createElement("h3")
+  name.textContent = basic.name;
+  child1.appendChild(name);
+
+  var email = document.createElement("a");
+  email.href = "vdivyamrrdc@gmail.com";
+  email.textContent = basic.gmail;
+  child1.appendChild(email);
+
+  var phone1 = document.createElement("h3");
+  phone1.textContent = basic.phone;
+  child1.appendChild(phone1);
+
+  var addr = document.createElement("h1");
+  addr.textContent = "address";
+  child1.appendChild(addr);
+
+  var hr = document.createElement("hr");
+  child1.appendChild(hr);
+
+  var address = document.createElement("P")
+  address.textContent = basic.address;
+  child1.appendChild(address);
+
+}
+function career(obj){
+  var heading = document.createElement("h2");
+  child2.appendChild(heading);
+  heading.textContent = "career objective";
+  var hline = document.createElement("hr");
+  child2.appendChild(hline);
+  var p = document.createElement("p");
+  p.textContent = obj.info;
+  child2.appendChild(p);
+
+}
+function education(edu){
+  var heading = document.createElement("h2");
+  child2.appendChild(heading);
+  heading.textContent = "Educational Qualifications";
+  var hline=document.createElement("hr");
+  child2.appendChild(hline);
+  var list=document.createElement("ul");
+  child2.appendChild(list);
+  for(var i=0; i<edu.length;i++){
+  var litem=document.createElement("li");
+  list.appendChild(litem);
+  litem.textContent=edu[i].course;
+  var college=document.createElement("p");
+  list.appendChild(college);
+  college.textContent=edu[i].college;
+  var data=document.createElement("P");
+  list.appendChild(data);
+  data.textContent=edu[i].data;
+}
+}
